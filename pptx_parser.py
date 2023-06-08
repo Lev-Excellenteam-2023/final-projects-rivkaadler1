9c92d02313d62ac219cc53ebdf0e6738f3c37521
rivkaadler1 <adlerrivka66@gmail.com>
2023-06-08 16:34:58 +0300

    adding a pptx-parser.py for parsing the presentations

--- pptx_parser.py
+++ pptx_parser.py
@@ -1,27 +1,30 @@
 from pptx import Presentation
 
 
-def parse_pptx_content_to_string_dict(pptx_path):
+def parse_content_to_string_dict(pptx_path: str):
     """
     Parses the content of a PowerPoint file and returns a dictionary with slide numbers as keys and
-    slide content as string values.
+    slide content (title, content, notes, and slide number) as string values.
 
     :param pptx_path: The path to the PowerPoint file.
-    :return: A dictionary with slide numbers as keys and slide content as string values.
+    :return: A dictionary with slide numbers as keys and slide
+    content (title, content, notes, and slide number) as string values.
     """
     presentation = Presentation(pptx_path)
     parsed_content = {}
 
     for i, slide in enumerate(presentation.slides, start=1):
-        slide_content = ""
+        title = slide.shapes.title.text.strip() if slide.shapes.title else ""
+        content = ''.join(run.text.strip() for shape in slide.shapes if shape.has_text_frame
+                          for paragraph in shape.text_frame.paragraphs for run in paragraph.runs)
 
-        for shape in slide.shapes:
-            if shape.has_text_frame:
-                for paragraph in shape.text_frame.paragraphs:
-                    for run in paragraph.runs:
-                        slide_content += run.text
-        # Only slides with non-empty content will be included
-        if slide_content:
+        notes_slide = slide.notes_slide
+        notes = ''.join(run.text.strip() for shape in notes_slide.shapes if shape.has_text_frame
+                        for paragraph in shape.text_frame.paragraphs for run in paragraph.runs)
+
+        if title or content or notes:
+            slide_content = f"title: {title}\ncontent: {content}\nnotes: {notes}\nslide number: {i}"
             parsed_content[i] = slide_content
 
     return parsed_content
+
