b5fe9e9b96f4611ac9ecafcd463ea0aa6a9d4bf7
rivkaadler1 <adlerrivka66@gmail.com>
2023-07-26 19:37:49 +0300

    changes to post method

--- web_api/app.py
+++ web_api/app.py
@@ -14,6 +14,23 @@ UPLOADS_FOLDER = os.environ.get("UPLOADS_FOLDER_PATH")
 OUTPUTS_FOLDER = os.environ.get("OUTPUTS_FOLDER_PATH")
 
 
+def generate_status_response(status: str, original_filename: str, timestamp, explanation):
+    """
+    Generates a JSON response to represent the status and relevant information related to a file processing operation.
+
+    Parameters:
+        status (str): A string representing the status of the file processing operation.
+        original_filename (str): The original filename of the processed file.
+        timestamp: A timestamp representing the time when the response is generated.
+        explanation (str): An optional string providing additional details or context about the status.
+
+    Return Value:
+        JSON Response: A JSON object containing the status information and relevant details in the response format.
+    """
+    return jsonify({'status': status, 'filename': original_filename, 'timestamp': timestamp,
+                    'explanation': explanation}), 200
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     """
@@ -59,36 +76,48 @@ def upload_file():
         return f'Error in upload endpoint: {str(e)}', 400
 
 
-# Define the status endpoint
 @app.route('/status/<uid>', methods=['GET'])
 def get_file_status(uid):
-    uploads_folder = 'data/uploads'
-    outputs_folder = 'data/outputs'
+    """
+    Retrieves the status and relevant information related to a specific file identified by its unique identifier (UID).
 
+    URL Endpoint:
+        /status/<uid>
+
+    HTTP Method:
+        GET
+
+    Parameters:
+        uid (str): The unique identifier (UID) of the file for which the status is to be retrieved.
+
+    Response:
+        Success (HTTP Status 200):
+            Content: JSON object containing the status information, original filename, timestamp, and explanation (if applicable).
+            Example: {"status": "pending", "filename": "example_file.txt", "timestamp": "2023-07-26 15:30:00", "explanation": null}
+
+        File Not Found (HTTP Status 404):
+            Content: JSON object indicating that the requested file was not found.
+            Example: {"status": "not found"}
+    """
     # Check if the file exists in the uploads folder
-    uploaded_file = next((filename for filename in os.listdir(uploads_folder) if uid in filename), None)
+    uploaded_file = next((filename for filename in os.listdir(UPLOADS_FOLDER) if uid == filename.split('_')[0]), None)
 
     if uploaded_file:
         # Extract the original filename and timestamp from the file name
-        timestamp, _, original_filename = uploaded_file.rsplit('_', 2)
-        original_filename = os.path.splitext(original_filename)[0]
-
-        return jsonify({'status': 'pending', 'filename': original_filename, 'timestamp': timestamp})
+        _, original_filename, timestamp = uploaded_file.rsplit('_', 2)
+        return generate_status_response('pending', original_filename, timestamp, None)
 
     # Check if the file exists in the outputs folder
-    output_file = next((filename for filename in os.listdir(outputs_folder) if uid in filename), None)
+    output_file = next((filename for filename in os.listdir(OUTPUTS_FOLDER) if uid == filename.split('_')[0]), None)
     if output_file:
         # Read the output JSON file and extract the explanation
-        output_file_path = os.path.join(outputs_folder, output_file)
+        output_file_path = os.path.join(OUTPUTS_FOLDER, output_file)
         with open(output_file_path, 'r') as file:
-            explanation = file.read()
-
+            data = json.load(file)
+            explanation = "".join(data[0])
         # Extract the filename and timestamp from the output file name
-        timestamp, _, original_filename = output_file.rsplit('_', 2)
-        original_filename = os.path.splitext(original_filename)[0]
-
-        return jsonify({'status': 'done', 'filename': original_filename, 'timestamp': timestamp,
-                        'explanation': explanation})
+        _, original_filename, timestamp = output_file.rsplit('_', 2)
+        return generate_status_response('done', original_filename, timestamp, explanation)
 
     # If the file doesn't exist in either folder, return appropriate response with 404 status
     return jsonify({'status': 'not found'}), 404
