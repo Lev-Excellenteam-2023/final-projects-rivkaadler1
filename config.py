32767f428e5001817552d41bfe3bb176103dd7f1
rivkaadler1 <adlerrivka66@gmail.com>
2023-07-28 01:14:53 +0300

    changing the name of the system folders

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-UPLOADS_FOLDER: "data/uploads"  # The path to the directory where uploaded files will be stored.
-OUTPUTS_FOLDER: "data/outputs"  # The path to the directory where output files will be saved.
+UPLOAD_FOLDER_PATH: 'The full path to the directory where uploaded files will be stored.'
+OUTPUT_FOLDER_PATH: 'The full path to the directory where output files will be saved.'
 API_URL: 'http://localhost'  # The URL of the Web API.
 API_PORT: 5000  # The port on which the Web API will listen for requests.
-FILENAME_FORMAT: "{timestamp}_{uid}_{original_filename}.extention"
--- interfaces.py
+++ interfaces.py
@@ -1,2 +1,2 @@
 API_UPLOAD_ENDPOINT: '/upload'  # The endpoint for file uploads in the Web API.
-API_STATUS_ENDPOINT: '/status/<uid>'  # The endpoint for checking the status of an upload in the
+API_STATUS_ENDPOINT: '/status/<uid>'  # The endpoint for checking the status of an uploaded file
